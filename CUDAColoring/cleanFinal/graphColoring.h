52419edb0556ff44c625561ef418700d46f19297
zetwal <zetwal@47f1d34c-260b-ee67-dd51-b23b07e135cf>
2010-04-23 03:59:18 +0000

    Clean version of the code; useless things (essentially dealing with adjacency matrices) have been purged

---
+++ CUDAColoring/cleanFinal/graphColoring.h
@@ -0,0 +1,34 @@
+#ifndef _GRAPHCOLORING_H_ 
+#define _GRAPHCOLORING_H_ 
+
+#include <stdlib.h> 
+#include <stdio.h> 
+#include <string.h> 
+#include <math.h> 
+#include <cuda_runtime_api.h> 
+#include <cuda.h> 
+#include <iostream>
+
+
+const long GRAPHSIZE = 10240;    // number of nodes
+const long NUMEDGES = 7500;    // number of edges 
+
+#define GRIDSIZE  4              // number of blocks 
+#define BLOCKSIZE 256            // number of threads in a block 
+
+const int SUBSIZE = GRAPHSIZE/(GRIDSIZE*BLOCKSIZE); 
+
+const int SUBSIZE_BOUNDARY = 256;
+
+#ifdef __cplusplus 
+	#define CHECK_EXT extern "C" 
+#else 
+	#define CHECK_EXT 
+#endif 
+
+
+CHECK_EXT void cudaGraphColoring(int *adjacentList, int *boundaryList, int *graphColors, int *degreeList, int *conflict, int boundarySize, int maxDegree);
+
+
+#endif // _GRAPHCOLORING_H_ 
+
