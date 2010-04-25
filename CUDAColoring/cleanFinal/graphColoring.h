87767eb0c2af5e0c3ca21e9c6247d9021454f573
zetwal <zetwal@47f1d34c-260b-ee67-dd51-b23b07e135cf>
2010-04-25 22:37:25 +0000

    Incomplete

--- CUDAColoring/cleanFinal/graphColoring.h
+++ CUDAColoring/cleanFinal/graphColoring.h
@@ -10,15 +10,17 @@
 #include <iostream>
 
 
-const long GRAPHSIZE = 10240;    // number of nodes
-const long NUMEDGES = 150000;    // number of edges 
+const long GRAPHSIZE = 1024;    // number of nodes
+const long NUMEDGES = 1024;    // number of edges 
 
 const int GRIDSIZE = 2;          // number of blocks 
-const int BLOCKSIZE = 512;       // number of threads in a block 
+const int BLOCKSIZE = 64;       // number of threads in a block 
 
 const int SUBSIZE = GRAPHSIZE/(GRIDSIZE*BLOCKSIZE); 
 
 const int SUBSIZE_BOUNDARY = 256;
+
+const int MAXDEGREE = 100;		// this is assumed to be true
 
 
 #ifdef __cplusplus 
@@ -28,7 +30,7 @@ const int SUBSIZE_BOUNDARY = 256;
 #endif 
 
 
-CHECK_EXT void cudaGraphColoring(int *adjacentList, int *boundaryList, int *graphColors, int *degreeList, int *conflict, int boundarySize, int maxDegree);
+CHECK_EXT void cudaGraphColoring(int *adjacentList, int *compactAdjacencyList, int *vertexStartList, int *boundaryList, int *graphColors, int *degreeList, int *conflict, int boundarySize, int maxDegree);
 
 
 #endif // _GRAPHCOLORING_H_ 
