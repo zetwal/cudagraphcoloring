9ee8c678fb8e7cef5a89e8bc2d6eb896125d273d
zetwal <zetwal@47f1d34c-260b-ee67-dd51-b23b07e135cf>
2010-04-29 14:59:41 +0000

    There was a bug that I forgot to correct in this version. No sweat though, it only makes the GPU speedup even better now.

--- CUDAColoring/tesla Good/graphColoring.cu
+++ CUDAColoring/tesla Good/graphColoring.cu
@@ -19,26 +19,27 @@ int __device__ saturation(int vertex, int *adjacencyList, int *graphColors, int 
 	int colors[256];
 	for (int j=0; j<256; j++)
 		colors[j] = 0;
-	
-	
+
+
 	for (int i=0; i<maxDegree; i++){
 		if (adjacencyList[vertex*maxDegree + i] < start)
 			continue;
-		
+
 		if (adjacencyList[vertex*maxDegree + i] > end)
 			break;
-		
+
 		if (adjacencyList[vertex*maxDegree + i] != -1)
-			colors[ graphColors[vertex] ] = 1;			// at each colored set the array to 1
+			//colors[ graphColors[vertex] ] = 1;			// at each colored set the array to 1
+			colors[ graphColors[adjacencyList[vertex*maxDegree + i]] ] = 1;			// at each colored set the array to 1
 		else
 			break;
 	}
-	
-	
+
+
 	for (int i=1; i<maxDegree+1; i++)					// count the number of 1's but skip uncolored
 		if (colors[i] == 1)
 			saturation++;
-	
+
 	return saturation;
 }
 
@@ -51,28 +52,28 @@ int __device__ color(int vertex, int *adjacencyList, int *graphColors, int maxDe
 	int colors[256];
 	for (int j=0; j<246; j++)
 		colors[j] = 0;
-	
+
 	
 	if (graphColors[vertex] == 0)
 		numColored++;
 	
 	for (int i=0; i<maxDegree; i++){						// set the index of the color to 1
-		
+
 		// Limits color checking to subgraph
-		/*
-		 if (adjacencyList[vertex*maxDegree + i] < start)
-		 continue;
-		 
-		 if (adjacencyList[vertex*maxDegree + i] > end)
-		 break;
-		 */
-		
+/*
+		if (adjacencyList[vertex*maxDegree + i] < start)
+			continue;
+
+		if (adjacencyList[vertex*maxDegree + i] > end)
+			break;
+*/
+
 		if (adjacencyList[vertex*maxDegree + i] != -1)
 			colors[  graphColors[  adjacencyList[vertex*maxDegree + i]  ]  ] = 1;
 		else 
 			break;
 	}
-	
+
 	
 	for (int i=1; i<maxDegree+1; i++)					// nodes still equal to 0 are unassigned
 		if (colors[i] != 1){
@@ -98,7 +99,7 @@ __global__ void colorGraph_SDO(int *adjacencyList, int *graphColors, int *degree
 	subGraphSize = sizeGraph/(gridDim.x * blockDim.x);
 	start = (sizeGraph/gridDim.x * blockIdx.x) + (subGraphSize * threadIdx.x);
 	end = start + subGraphSize;
-	
+
 	while (numColored < subGraphSize){
 		max = -1;
 		
@@ -106,18 +107,18 @@ __global__ void colorGraph_SDO(int *adjacencyList, int *graphColors, int *degree
 			if (graphColors[i] == 0)			// not colored
 			{
 				satDegree = saturation(i,adjacencyList,graphColors, maxDegree, start, end);
-				
+
 				if (satDegree > max){
 					max = satDegree;
 					index = i;				
 				}
-				
+
 				if (satDegree == max){
 					if (degree(i,degreeList) > degree(index,degreeList))
 						index = i;
 				}
 			}
-			
+
 			numColored = color(index,adjacencyList,graphColors, maxDegree, numColored, start, end);
 		}
 	}
@@ -139,17 +140,17 @@ __global__ void colorGraph_FF(int *adjacencyListD, int *colors, int size, int ma
 	start = (size/gridDim.x * blockIdx.x) + (subGraphSize * threadIdx.x);
 	end = start + subGraphSize;
 	
-	
+
 	int degreeArray[100];
 	for(i=start; i<end; i++)
 	{
 		for(j=0; j<maxDegree; j++)
 			degreeArray[j] = j+1;
-		
-		
+
+
 		for (j=0; j<maxDegree; j++){
 			int vertexNeigh = i*maxDegree + j;
-			
+
 			if (adjacencyListD[vertexNeigh] != -1){
 				if (colors[ adjacencyListD[vertexNeigh] ] != 0)
 					degreeArray[ colors[adjacencyListD[vertexNeigh]] -1 ] = 0;
@@ -158,7 +159,7 @@ __global__ void colorGraph_FF(int *adjacencyListD, int *colors, int size, int ma
 				break;
 		}
 		
-		
+
 		for(j=0; j<maxDegree; j++)
 			if(degreeArray[j] != 0){
 				colors[i] = degreeArray[j];
@@ -205,15 +206,15 @@ void cudaGraphColoring(int *adjacentList, int *boundaryList, int *graphColors, i
 	
 	cudaEvent_t start_col, start_confl, stop_col, stop_confl, start_mem, stop_mem;         
     float elapsedTime_memory, elapsedTime_col, elapsedTime_confl; 
-	
-	
-	
-	//-------------- memory transfer -----------------!
+
+
+
+//-------------- memory transfer -----------------!
 	cudaEventCreate(&start_mem); 
     cudaEventCreate(&stop_mem); 
     cudaEventRecord(start_mem, 0); 
 	
-	
+
 	cudaMalloc((void**)&adjacentListD, graphSize*maxDegree*sizeof(int));
 	cudaMalloc((void**)&colorsD, graphSize*sizeof(int));
 	cudaMalloc((void**)&conflictD, boundarySize*sizeof(int));
@@ -230,8 +231,8 @@ void cudaGraphColoring(int *adjacentList, int *boundaryList, int *graphColors, i
     cudaEventSynchronize(stop_mem); 
 	
 	
-	
-	
+
+
 	dim3 dimGrid_col(GRIDSIZE);
 	dim3 dimBlock_col(BLOCKSIZE);
 	
@@ -239,9 +240,9 @@ void cudaGraphColoring(int *adjacentList, int *boundaryList, int *graphColors, i
 	dim3 dimBlock_confl(blocksize);
 	
 	
-	
-	
-	//-------------- Sequential Graph coloring -----------------!
+
+
+//-------------- Sequential Graph coloring -----------------!
 	cudaEventCreate(&start_col); 
     cudaEventCreate(&stop_col); 
     cudaEventRecord(start_col, 0); 
@@ -253,12 +254,12 @@ void cudaGraphColoring(int *adjacentList, int *boundaryList, int *graphColors, i
 	
 	cudaEventRecord(stop_col, 0); 
     cudaEventSynchronize(stop_col); 
+
 	
-	
-	
-	
-	
-	//-------------- Conflict resolution -----------------!
+
+
+
+//-------------- Conflict resolution -----------------!
 	cudaEventCreate(&start_confl); 
     cudaEventCreate(&stop_confl); 
     cudaEventRecord(start_confl, 0); 
@@ -268,24 +269,24 @@ void cudaGraphColoring(int *adjacentList, int *boundaryList, int *graphColors, i
 	cudaEventRecord(stop_confl, 0); 
     cudaEventSynchronize(stop_confl); 
 	
-	
-	
-	
-	
-	//-------------- Cleanup -----------------!
+
+
+
+
+//-------------- Cleanup -----------------!
 	cudaMemcpy(graphColors, colorsD, graphSize*sizeof(int), cudaMemcpyDeviceToHost);
 	cudaMemcpy(conflict, conflictD, boundarySize*sizeof(int), cudaMemcpyDeviceToHost);
-	
-	
-	
+
+
+
 	cudaEventElapsedTime(&elapsedTime_memory, start_mem, stop_mem); 
 	cudaEventElapsedTime(&elapsedTime_col, start_col, stop_col); 
 	cudaEventElapsedTime(&elapsedTime_confl, start_confl, stop_confl); 
-	
+
 	cout << "GPU timings ~ Memory transfer: " << elapsedTime_memory  << " ms     Coloring: " 
-	<< elapsedTime_col << " ms    Conflict: " << elapsedTime_confl << " ms" << endl; 
-	
-	
+		 << elapsedTime_col << " ms    Conflict: " << elapsedTime_confl << " ms" << endl; 
+
+
 	cudaFree(adjacentListD);
 	cudaFree(colorsD);
 	cudaFree(conflictD);
